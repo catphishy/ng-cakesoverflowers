ceafb5f726bb24f919ad611019182085c7ded28a
Jemina Sy <jemina.c.sy@gmail.com>
2024-02-12 18:33:13 +0800

    our-cakes banner

--- src/app/Component/home/home.component.ts
+++ src/app/Component/home/home.component.ts
@@ -5,12 +5,12 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <div class="container container-blend p-5">
-      <h3>Great Taste in every bite</h3>
-      <p class="text-primary-emphasis">Not sure what to buy for your special some? It's an easy choice - <i>Cakes Over Flowers</i></p>
+    <div class="container container-blend p-5" id="main-cover">
+      <h3 class="text-secondary-emphasis">Great Taste in every bite</h3>
+      <p class="text-secondary-emphasis">Not sure what to buy for your special some? It's an easy choice - <i>Cakes Over Flowers!</i></p>
     </div>
-    <section class="p-5">
-      <h3 class="text-center mb-3">Bestsellers</h3>
+    <section class="p-5" style="border-bottom: 3px solid #8E6944">
+      <h3 class="text-center text-secondary-emphasis mb-3">Bestsellers</h3>
       <div class="row">
         <div class="col-md-3">
           <div class="card card-body border border-0 mb-3 p-6" >
@@ -46,6 +46,20 @@ import { Component } from '@angular/core';
         </div>
       </div>
     </section>
+    <div class="position-relative container container-blend p-5" id="cakes-cover">
+      <h2 class="text-center text-secondary-emphasis mt-2">Our Cakes</h2>
+      <br>
+      <h6 class="text-center lead text-secondary-emphasis">Made for something special</h6>
+      <div class="position-absolute top-50 start-50 translate-middle mt-4">
+        <button type="button" class="btn btn-primary">Order Now</button>
+      </div>
+      
+
+    </div>
+    <div class="container container-blend p-5" id="testimonials">
+      <h3 class="text-secondary-emphasis">Great Taste in every bite</h3>
+      <p class="text-secondary-emphasis">Not sure what to buy for your special some? It's an easy choice - <i>Cakes Over Flowers!</i></p>
+    </div>
   `,
   styleUrl: './home.component.css'
 })
