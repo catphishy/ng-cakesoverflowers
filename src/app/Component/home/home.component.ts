f6fd8c9a863fbcdaac3681dfd46e5c7f930e3f5e
Jemina Sy <jemina.c.sy@gmail.com>
2024-02-12 21:07:03 +0800

    home page partially done

--- src/app/Component/home/home.component.ts
+++ src/app/Component/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   template: `
     <div class="container container-blend p-5" id="main-cover">
       <h3 class="text-secondary-emphasis">Great Taste in every bite</h3>
-      <p class="text-secondary-emphasis">Not sure what to buy for your special some? It's an easy choice - <i>Cakes Over Flowers!</i></p>
+      <p class="text-secondary-emphasis">Not sure what to buy for your special someone? It's an easy choice - <i>Cakes Over Flowers!</i></p>
     </div>
     <section class="p-5" style="border-bottom: 3px solid #8E6944">
       <h3 class="text-center text-secondary-emphasis mb-3">Bestsellers</h3>
@@ -53,13 +53,71 @@ import { Component } from '@angular/core';
       <div class="position-absolute top-50 start-50 translate-middle mt-4">
         <button type="button" class="btn btn-primary">Order Now</button>
       </div>
-      
-
     </div>
     <div class="container container-blend p-5" id="testimonials">
-      <h3 class="text-secondary-emphasis">Great Taste in every bite</h3>
-      <p class="text-secondary-emphasis">Not sure what to buy for your special some? It's an easy choice - <i>Cakes Over Flowers!</i></p>
+      <h3 class="text-center text-secondary-emphasis mt-3">Testimonials</h3>
+      <p class="text-center text-secondary-emphasis m-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p class="text-center text-secondary"><strong>- JOHN DOE</strong></p>
     </div>
+    <div class="position-relative container container-blend p-5" id="custom-cake">
+      <h2 class="text-center text-secondary-emphasis mt-2">Customize your own cake</h2>
+      <br>
+      <h6 class="text-center lead text-secondary-emphasis">Let us bake your ideas to life</h6>
+      <div class="position-absolute top-50 start-50 translate-middle mt-4">
+        <button type="button" class="btn btn-primary">Order Now</button>
+      </div>
+    </div>
+    <section class="p-5">
+      <div class="row">
+        <div class="col-md-4 p-4 pb-0">
+          <div class="card card-body border border-0 mb-3 p-6" >
+            <h4 class="text-secondary-emphasis mb-3">NEWSLETTER</h4>
+            <form>
+              <div class="form-group mb-3">
+                <p>Stay updated with our news and promos</p>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+              </div>
+              <button type="submit" class="btn btn-primary">Subscribe</button>
+            </form>
+          </div>
+        </div>
+        <div class="col-md-4 p-4 pb-0">
+          <div class="card card-body border border-0 mb-3 p-6" >
+            <h4 class="text-secondary-emphasis mb-3">CONNECT</h4>
+            <a class="mb-2">Home</a>
+            <a class="mb-2">Shop</a>
+            <a class="mb-2">Customize</a>
+          </div>
+        </div>
+        <div class="col-md-4 p-4 pb-0" style="">
+          <div class="card card-body border border-0 mb-3 p-6" style="object-fit: cover;">
+            <h4 class="text-secondary-emphasis mb-3">EXPLORE</h4>
+            <div class="mb-2">
+              <img src="../../../assets/contact-solid-24.png" style="width:22px; height:22px; display: inline-block; vertical-align: middle;">
+              <span style="display: inline-block; vertical-align: middle;">&nbsp;&nbsp;Contact Us</span>
+            </div>
+            <div class="mb-2">
+              <img src="../../../assets/envelope-solid-24.png" style="width:22px; height:22px; display: inline-block; vertical-align: middle;">
+              <span style="display: inline-block; vertical-align: middle;">&nbsp;&nbsp;cakesoverflowers&#64;gmail.com</span>
+            </div>
+            <div class="mb-2">
+              <img src="../../../assets/phone-solid-24.png" style="width:22px; height:22px; display: inline-block; vertical-align: middle;">
+              <span style="display: inline-block; vertical-align: middle;">&nbsp;&nbsp;(63)912 345 6789</span>
+            </div>
+            <div class="mb-2">
+              <img src="../../../assets/instagram-alt-logo-24.png" style="width:22px; height:22px; display: inline-block; vertical-align: middle;">
+              <span style="display: inline-block; vertical-align: middle;">&nbsp;&nbsp;cakes.over.flowers</span>
+            </div>
+            <div class="mb-2">
+              <img src="../../../assets/time-five-solid-24.png" style="width:22px; height:22px; display: inline-block; vertical-align: middle;">
+              <span style="display: inline-block; vertical-align: middle;">&nbsp;&nbsp;9AM - 10PM (GMT +8)</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      
+    </section>
   `,
   styleUrl: './home.component.css'
 })
