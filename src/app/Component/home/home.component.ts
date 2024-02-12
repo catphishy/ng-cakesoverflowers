e6401daeadef767060e890cd3037f3d02a9f3748
Jemina Sy <jemina.c.sy@gmail.com>
2024-02-12 18:01:02 +0800

    bestsellers

--- src/app/Component/home/home.component.ts
+++ src/app/Component/home/home.component.ts
@@ -14,22 +14,34 @@ import { Component } from '@angular/core';
       <div class="row">
         <div class="col-md-3">
           <div class="card card-body border border-0 mb-3 p-6" >
-            <img src="../../../assets/blueberry-cheesecake.jpg">
+            <img src="../../../assets/blueberry-cheesecake.jpg" style="object-fit: fill">
+            <h5 class="text-center text-secondary-emphasis mt-2">Blueberry Cheesecake</h5>
+            <p class="text-center text-secondary-emphasis"><small>A buttery graham cracker crust, a creamy cheesecake center, and a tangy blueberry topping</small></p>
+            <button type="button" class="btn btn-primary">View</button>          
           </div>
         </div>
         <div class="col-md-3">
           <div class="card card-body border border-0 mb-3 p-6" >
             <img src="../../../assets/carrot-walnut.jpg">
+            <h5 class="text-center text-secondary-emphasis mt-2">Carrot Walnut Cake</h5>
+            <p class="text-center text-secondary-emphasis"><small>A buttery graham cracker crust, a creamy cheesecake center, and a tangy blueberry topping</small></p>
+            <button type="button" class="btn btn-primary">View</button>  
           </div>
         </div>
         <div class="col-md-3">
           <div class="card card-body border border-0 mb-3 p-6" >
             <img src="../../../assets/strawberry-tres-leches.jpg">
+            <h5 class="text-center text-secondary-emphasis mt-2">Strawberry Tres Leches</h5>
+            <p class="text-center text-secondary-emphasis"><small>A buttery graham cracker crust, a creamy cheesecake center, and a tangy blueberry topping</small></p>
+            <button type="button" class="btn btn-primary">View</button>  
           </div>
         </div>
         <div class="col-md-3">
-          <div class="card card-body border border-0 mb-3 p-6" >
+          <div class="card card-body border border-0 mb-3 p-6" style="object-fit: cover;">
             <img src="../../../assets/matcha-tres-leches.jpg">
+            <h5 class="text-center text-secondary-emphasis mt-2">Matcha Tres Leches</h5>
+            <p class="text-center text-secondary-emphasis"><small>A buttery graham cracker crust, a creamy cheesecake center, and a tangy blueberry topping</small></p>
+            <button type="button" class="btn btn-primary">View</button>
           </div>
         </div>
       </div>
